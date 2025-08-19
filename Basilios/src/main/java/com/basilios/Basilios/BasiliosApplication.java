2df79866c3c6e23c05750177b52ed93555aae790
pedromorais03 <pedromorais3012@gmail.com>
2025-08-19 19:13:27 -0300

    Projeto Spring criado

---
+++ Basilios/src/main/java/com/basilios/Basilios/BasiliosApplication.java
@@ -0,0 +1,13 @@
+package com.basilios.Basilios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasiliosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasiliosApplication.class, args);
+	}
+
+}
---
+++ Basilios/src/test/java/com/basilios/Basilios/BasiliosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basilios.Basilios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasiliosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
