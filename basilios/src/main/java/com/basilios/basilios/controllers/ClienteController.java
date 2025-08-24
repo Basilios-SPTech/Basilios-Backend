e32092e576f81bd2492616c3bf19c6372637d0c9
pedromorais03 <pedromorais3012@gmail.com>
2025-08-23 22:44:05 -0300

    Created insert and verify paths for client

--- basilios/src/main/java/com/basilios/basilios/controllers/ClienteController.java
+++ basilios/src/main/java/com/basilios/basilios/controllers/ClienteController.java
@@ -1,16 +1,78 @@
 package com.basilios.basilios.controllers;
 
+import com.basilios.basilios.model.Cliente;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/cliente")
+@RequestMapping("/client")
 public class ClienteController {
 
+    List<Cliente> clientes = new ArrayList<>();
+
     @GetMapping
-    public ResponseEntity<Void> getCliente(){
-        return ResponseEntity.status(200).build();
+    public ResponseEntity<List<Cliente>> getClient(){
+        return ResponseEntity.status(200).body(clientes);
+    }
+
+    @PostMapping("/verify-client")
+    public ResponseEntity<Cliente> verifyClient(@RequestBody Map<String, String> credentials){
+        try {
+            String username = credentials.get("username");
+            String password = credentials.get("password");
+
+            if(username == null  || username.trim().isEmpty() ||
+                password == null || password.trim().isEmpty()){
+                return ResponseEntity.status(400).build();
+            }
+
+            Cliente clientFound = clientes.stream()
+                    .filter(cliente -> username.equals(cliente.getUsername()) &&
+                            password.equals(cliente.getPassword()))
+                    .findFirst()
+                    .orElse(null);
+
+            if(clientFound != null){
+                return ResponseEntity.status(200).body(clientFound);
+            }else{
+                return ResponseEntity.status(401).build();
+            }
+
+        }catch (Exception e){
+            return ResponseEntity.status(500).build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insertClient(@RequestBody Cliente client){
+        try{
+            if(client == null){
+                return ResponseEntity.status(400).build();
+            }
+
+            if (client.getUsername() == null || client.getUsername().trim().isEmpty()) {
+                return ResponseEntity.status(400).build();
+            }
+
+            if (client.getPassword() == null || client.getPassword().trim().isEmpty()) {
+                return ResponseEntity.status(400).build();
+            }
+
+            boolean clientAlreadyExists = clientes.stream()
+                    .anyMatch(c -> client.getUsername().equals(c.getUsername()));
+
+            if(clientAlreadyExists){
+                return ResponseEntity.status(409).build();
+            }
+
+            clientes.add(client);
+            return ResponseEntity.status(201).build();
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build();
+        }
     }
 }
