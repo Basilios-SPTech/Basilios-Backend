c468a68764df1a0e4e0171fc1a7909d5f5ab5f54
joaoohi <joao.osantos@sptech.school>
2025-12-10 17:40:00 -0300

    Inserindo todos os produtos e fotos da Basilios na tabela product

--- basilios/src/main/resources/data.sql
+++ basilios/src/main/resources/data.sql
@@ -162,11 +162,6 @@ VALUES
      0, 'X-Burger Salada', 30.00, 'BEEF', '2025-11-26 21:12:33.519729',
      '/uploads/fbda5c68-93af-4b77-94ae-932d2154d0f7.jpg'),
 
-    (17, 'BURGER', '2025-11-26 21:13:23.849419',
-     'Um pão de hambúrguer macio, recheado com um hambúrguer de 110g suculento e finalizado com cebola frita crocante que dá aquele toque especial de sabor.',
-     0, 'X-burger acebolado', 30.00, 'BEEF', '2025-11-26 21:13:23.849419',
-     '/uploads/3dba0349-208f-4fe5-8f76-2e548319ee3f.jpg'),
-
     (18, 'BURGER', '2025-11-26 21:14:16.939855',
      'Um pão de hambúrguer macio, recheado com uma carne de 120g suculenta, coberta por queijo derretido, acompanhada de tomate fresco, alface crocante e finalizada com a cremosidade da nossa maionese caseira. Uma combinação clássica e irresistível!',
      0, 'X-Burger Bacon Salada', 36.00, 'BEEF', '2025-11-26 21:14:16.939855',
@@ -174,6 +169,76 @@ VALUES
 
     (19, 'BURGER', '2025-11-26 21:14:57.711666',
      'Um pão de hambúrguer macio, recheado com um hambúrguer de 110g suculento, coberto por queijo derretido e finalizado com o sabor marcante do molho tártaro. Simples, clássico e irresistível!',
-     0, 'X-burger tártaro', 30.00, 'BEEF', '2025-11-29 19:36:03.066068',
-     '/uploads/37a428cc-b44d-4fae-aa35-d71e162e926d.jpg');
+     0, 'X-burger tártaro', 30.00, 'BEEF', '2025-12-02 21:11:32.647936',
+     '/uploads/37a428cc-b44d-4fae-aa35-d71e162e926d.jpg'),
+
+    (21, 'BURGER', '2025-12-02 21:18:22.907896',
+     'O clássico X-Bacon Egg com um hambúrguer suculento, bacon crocante e ovo saboroso, acompanhado de salada fresca, presunto e cebola acebolada que trazem equilíbrio e intensidade de sabor em cada mordida.',
+     0, 'Aclimação', 39.00, 'BEEF', '2025-12-02 21:18:22.909892',
+     '/uploads/8d8e343e-e134-44bd-9903-958cd4e4e538.jpg'),
+
+    (24, 'BURGER', '2025-12-07 03:02:23.961220',
+     'Beirute médio de frango com mussarela e presunto, acompanhado de alface fresca, tomate e maionese cremosa.',
+     0, 'Beirute médio (Frango)', 35.00, 'CHICKEN', '2025-12-07 03:02:23.969262',
+     NULL),
+
+    (25, 'BURGER', '2025-12-07 03:05:44.842412',
+     'Beirute grande de frango com mussarela e presunto, alface fresca, tomate e maionese cremosa, em porção mais generosa.',
+     0, 'Beirute Grande (Frango)', 43.00, 'CHICKEN', '2025-12-07 03:05:44.845900',
+     NULL),
+
+    (26, 'BURGER', '2025-12-07 03:06:45.040924',
+     'Beirute médio de filé mignon com mussarela e presunto, alface fresca, tomate e maionese cremosa.',
+     0, 'Beirute médio (Filé Mignon)', 41.00, 'BEEF', '2025-12-07 03:06:45.040924',
+     NULL),
+
+    (27, 'BURGER', '2025-12-07 03:07:37.775835',
+     'Beirute grande de filé mignon com mussarela e presunto, alface fresca, tomate e maionese cremosa, em porção mais generosa.',
+     0, 'Beirute Grande (Filé Mignon)', 52.00, 'BEEF', '2025-12-07 03:07:37.775835',
+     NULL),
+
+    (28, 'BURGER', '2025-12-07 03:18:31.326171',
+     'Hot dog premium com duas salsichas, bacon crocante, cheddar, Catupiry, maionese e batata palha.',
+     0, 'Dog Premium', 23.00, 'BEEF', '2025-12-07 03:18:31.334135',
+     '/uploads/54d0d658-477a-4b82-97a4-224c092c1dcd.jpg'),
+
+    (29, 'BURGER', '2025-12-07 03:19:58.206518',
+     'Hot dog com pão macio, duas salsichas, maionese cremosa e batata palha crocante.',
+     0, 'Dog Maionese Batata Palha', 18.00, 'BEEF', '2025-12-07 03:19:58.206518',
+     NULL),
+
+    (30, 'SIDE', '2025-12-07 03:22:46.316517',
+     'Deliciosa Fritas',
+     0, 'Batata Frita', 14.00, 'FRIES', '2025-12-07 03:22:46.316517',
+     '/uploads/ea655094-3f82-4cd3-8b35-20c750bd4347.jpg'),
+
+    (31, 'SIDE', '2025-12-07 03:25:31.705628',
+     'Batata Crinkle bem crocante!',
+     0, 'Batata Crinkle', 14.00, 'FRIES', '2025-12-07 03:25:31.705628',
+     '/uploads/d5b28c84-2f30-44cd-8a95-4fb5ff887de5.jpg'),
+
+    (32, 'SIDE', '2025-12-07 03:27:02.855919',
+     'Deliciosas fritas c/ Cheddar e Bacon',
+     0, 'Batata Frita c/ Cheddar e Bacon', 22.00, 'FRIES', '2025-12-07 03:27:02.869810',
+     '/uploads/1dcb040e-71d5-4fe9-b024-7c263fc2fd46.jpg'),
+
+    (33, 'SIDE', '2025-12-07 03:28:05.194697',
+     'Anéis de cebola empanado',
+     0, 'Onion Rings', 24.00, 'ONION_RINGS', '2025-12-07 03:28:05.194697',
+     '/uploads/0a99630c-4b77-4991-8bf6-7dc66ef016ea.jpg'),
+
+    (34, 'SIDE', '2025-12-07 03:29:11.554788',
+     'Batata em forma de canoa',
+     0, 'Batata Canoa', 21.00, 'FRIES', '2025-12-07 03:29:11.554788',
+     '/uploads/b35a327e-c8a3-409f-84cb-61aa5aaed243.jpg'),
+
+    (35, 'COMBO', '2025-12-07 03:32:01.064697',
+     'X-salada (pão de hamburguer, hamburguer 110g, queijo, alface, tomate e maionese) + fritas 100g + refrigerante lata 350ml',
+     0, 'Combo X-Salada', 42.00, NULL, '2025-12-08 14:48:25.307498',
+     '/uploads/b12edbe8-111b-4f1e-97a5-6057f1e86ed0.jpg'),
+
+    (36, 'COMBO', '2025-12-07 03:34:03.130399',
+     'Combo X-Bacon salada com hambúrguer suculento, bacon crocante, queijo derretido, alface, tomate e maionese, acompanhado de batata frita e refrigerante.',
+     0, 'Combo X-Bacon Salada', 45.00, NULL, '2025-12-07 03:34:03.133372',
+     '/uploads/9ff9351e-bc05-47c4-93e5-18d489be2584.jpg');
 
